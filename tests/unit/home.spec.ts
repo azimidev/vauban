2f06336afd8cf87368c49d035d84f4f701e76fe0
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-06-09 21:28:56 +0100

    reformat

--- tests/unit/home.spec.ts
+++ tests/unit/home.spec.ts
@@ -1,34 +1,34 @@
-import { mount } from '@vue/test-utils';
-import Home from '@/views/Home.vue';
-import { localVue, store, characters } from '../testUtils';
+import { mount } from "@vue/test-utils";
+import Home from "@/views/Home.vue";
+import { localVue, store, characters } from "../testUtils";
 
 let wrapper: any;
 
-describe('Home.vue', () => {
-	beforeEach(() => {
-		wrapper = mount(Home, {
-			store,
-			localVue,
-			data() {
-				return {};
-			},
-		});
-	});
+describe("Home.vue", () => {
+  beforeEach(() => {
+    wrapper = mount(Home, {
+      store,
+      localVue,
+      data() {
+        return {};
+      }
+    });
+  });
 
-	test('should render without error', (): void => {
-		expect(wrapper).toBeTruthy();
-	});
+  test("should render without error", (): void => {
+    expect(wrapper).toBeTruthy();
+  });
 
-	test('should display all character', () => {
-		for (let i = 0; i <= 9; i++) {
-			expect(wrapper.text()).toMatch(characters[i].name);
-			expect(wrapper.text()).toMatch(characters[i].height);
-			expect(wrapper.text()).toMatch(characters[i].mass);
-			expect(wrapper.text()).toMatch(characters[i].hair_color);
-			expect(wrapper.text()).toMatch(characters[i].skin_color);
-			expect(wrapper.text()).toMatch(characters[i].eye_color);
-			expect(wrapper.text()).toMatch(characters[i].birth_year);
-			expect(wrapper.text()).toMatch(characters[i].gender);
-		}
-	});
+  test("should display all character", () => {
+    for (let i = 0; i <= 9; i++) {
+      expect(wrapper.text()).toMatch(characters[i].name);
+      expect(wrapper.text()).toMatch(characters[i].height);
+      expect(wrapper.text()).toMatch(characters[i].mass);
+      expect(wrapper.text()).toMatch(characters[i].hair_color);
+      expect(wrapper.text()).toMatch(characters[i].skin_color);
+      expect(wrapper.text()).toMatch(characters[i].eye_color);
+      expect(wrapper.text()).toMatch(characters[i].birth_year);
+      expect(wrapper.text()).toMatch(characters[i].gender);
+    }
+  });
 });
--- tests/unit/navbar.spec.ts
+++ tests/unit/navbar.spec.ts
@@ -1,22 +1,21 @@
-import { mount } from '@vue/test-utils';
-import NavBar from '@/components/NavBar.vue';
-import { localVue, store, characters } from '../testUtils';
+import { mount } from "@vue/test-utils";
+import NavBar from "@/components/NavBar.vue";
+import { localVue, store, characters } from "../testUtils";
 
 let wrapper: any;
 
-describe('NavBar.vue', () => {
-	beforeEach(() => {
-		wrapper = mount(NavBar, {
-			store,
-			localVue,
-			data() {
-				return {};
-			},
-		});
-	});
+describe("NavBar.vue", () => {
+  beforeEach(() => {
+    wrapper = mount(NavBar, {
+      store,
+      localVue,
+      data() {
+        return {};
+      }
+    });
+  });
 
-	test('should render without error', (): void => {
-		expect(wrapper).toBeTruthy();
-	});
-
+  test("should render without error", (): void => {
+    expect(wrapper).toBeTruthy();
+  });
 });
